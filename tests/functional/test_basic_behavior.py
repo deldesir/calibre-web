9c92d2117d8877b3e35f8a1c84b54bbc5533658c
deldesir <blondel.md@gmail.com>
2025-01-16 20:52:51 -0500

    Update test_basic_behavior.py

--- tests/functional/test_basic_behavior.py
+++ tests/functional/test_basic_behavior.py
@@ -1,34 +1,31 @@
 """Basic behavior feature tests."""
 import pytest
-from pytest_bdd import (
-    given,
-    scenario,
-    then,
-    when,
-)
-
+from pytest_bdd import given, scenario, then, when
+from splinter import Browser
+from splinter.config import Config
 import os
-import subprocess 
 from urllib.parse import urljoin
-import time
+
 
 @pytest.fixture(scope='session')
-def splinter_headless():
-    """Override splinter headless option."""
-    if os.environ['HEADLESS'] == "true":
-        return True
-    else:
-        return False 
+def splinter_config():
+    """Return a Splinter Config object."""
+    headless = os.environ.get('HEADLESS', 'true').lower() == 'true'
+    return Config(headless=headless)
+
 
 @pytest.fixture(scope='session')
-def splinter_webdriver():
-    """Override splinter webdriver name."""
-    return 'chrome'
+def splinter_browser(splinter_config):
+    """Provide a browser instance using Splinter Config."""
+    with Browser('chrome', config=splinter_config) as browser:
+        yield browser
+
 
 @pytest.fixture
 def step_context():
     """Fixture to save information to use through steps."""
     return {}
+
 
 @scenario('basic_behavior.feature', 'Home Page')
 def test_home_page():
@@ -42,24 +39,26 @@ def _(step_context):
 
 
 @when('I go to the home page')
-def _(browser, step_context):
+def _(splinter_browser, step_context):
     """I go to the home page."""
-    url = urljoin("".join(['http://', str(step_context['ip_address'])]), '/')
-    browser.visit(url)
+    url = urljoin(f"http://{step_context['ip_address']}", '/')
+    splinter_browser.visit(url)
 
 
 @then('I should not see the error message')
-def _(browser, step_context):
+def _(splinter_browser):
     """I should not see the error message."""
 
+
 @then('see homepage information')
-def _(browser):
+def _(splinter_browser):
     """see homepage information."""
     print("!!!!!!!")
-    print(browser.title)
-    print(browser.url)
+    print(splinter_browser.title)
+    print(splinter_browser.url)
     print("!!!!!!!")
-    assert browser.is_text_present('Books'), 'Book test'
+    assert splinter_browser.is_text_present('Books'), 'Book test'
+
 
 @scenario('basic_behavior.feature', 'Login')
 def test_login():
@@ -67,30 +66,31 @@ def test_login():
 
 
 @given('I visit the calibre web homepage')
-def _(browser, step_context):
+def _(splinter_browser, step_context):
     """I visit the calibre web homepage."""
     step_context['ip_address'] = 'localhost:8083'
-    url = urljoin("".join(['http://', str(step_context['ip_address'])]), '/')
-    browser.visit(url)
+    url = urljoin(f"http://{step_context['ip_address']}", '/')
+    splinter_browser.visit(url)
 
 
 @when('I login with valid credentials')
-def _(browser, step_context):
+def _(splinter_browser):
     """I login with valid credentials."""
-    browser.fill('username', 'Admin')
-    browser.fill('password', 'changeme')
-    button = browser.find_by_name('submit')
+    splinter_browser.fill('username', 'Admin')
+    splinter_browser.fill('password', 'changeme')
+    button = splinter_browser.find_by_name('submit')
     # Interact with elements
     button.click()
 
 
 @then('I should see the success message')
-def _(browser, step_context):
+def _(splinter_browser):
     """I should see the success message."""
-    assert browser.is_text_present('You are now logged in as:'), 'Login successful'
+    assert splinter_browser.is_text_present('You are now logged in as:'), 'Login successful'
+
 
 @then('see the information for logged users')
-def _(browser):
+def _(splinter_browser):
     """see the information for logged users"""
-    assert browser.is_text_present('Books'), 'Expected "Books" text to be visible on the home page' 
-    assert browser.is_text_present('Download to IIAB'), 'Expected "Download to IIAB" button for logged users' 
+    assert splinter_browser.is_text_present('Books'), 'Expected "Books" text to be visible on the home page'
+    assert splinter_browser.is_text_present('Download to IIAB'), 'Expected "Download to IIAB" button for logged users'
